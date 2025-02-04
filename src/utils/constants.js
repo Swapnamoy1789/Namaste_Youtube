34741e3726eccfbdcd61c6dbe89a47f01c3eb76b
Swapnamoy1789 <118628863+Swapnamoy1789@users.noreply.github.com>
2025-02-04 22:40:47 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY="AIzaSyCS8BSQJZB3z6pBKhGd8zAVv2gTVqlCTgM";
+export const GOOGLE_API_KEY="AIzaSyCvzKz2q5cUGTt0E775gYhueYtwKby-tBY";
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
 
